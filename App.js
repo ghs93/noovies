92e9be975577215629c485b614ce35d79ada4484
hoseong <rksghtjd@gmail.com>
2021-12-06 10:44:55 +0900

    AppLoading part Three(1)
    Non Hook

--- App.js
+++ App.js
@@ -5,18 +5,33 @@ import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 
+/**
+ startAsync에서 asset을 preload하는 작업 외의 작업이 필요할 경우
+ 여러 작업을 배열로 만든 뒤 한번에 await Promise 한다.
+ */
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    //Font 사용
-    await Font.loadAsync(Ionicons.font);
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./my-face.jpeg"),
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
 
-    //Local Image
-    // await Asset.loadAsync(require(".imageName.jpeg"))
-
-    //Web or Server Image
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
