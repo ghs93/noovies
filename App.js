46a13f986810b45555dd84c7123f654cc6cd55c3
hoseong <rksghtjd@gmail.com>
2021-12-06 10:51:01 +0900

    AppLoading part Three(2)
    Use Hook

--- App.js
+++ App.js
@@ -3,44 +3,18 @@ import React, { useState } from "react";
 import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 
 /**
- startAsync에서 asset을 preload하는 작업 외의 작업이 필요할 경우
- 여러 작업을 배열로 만든 뒤 한번에 await Promise 한다.
+ 단순히 asset을 preload하는 작업일 경우 Hook을 사용하는 것을 추천
+ 단, Image.prefetch 또는 DB호출 등 다른 작업은 불가
  */
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
-
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./my-face.jpeg"),
-      "https://reactnative.dev/img/oss_logo.png",
-    ]);
-
-    await Promise.all([...fonts, ...images]);
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./my-face.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
 
   return <Text>We are dong loading!</Text>;
