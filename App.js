ed729caea34bae932eda46bba95df6946c2548ac
hoseong <rksghtjd@gmail.com>
2021-12-06 13:11:38 +0900

    Tab Navigation

--- App.js
+++ App.js
@@ -4,11 +4,8 @@ import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
-
-/**
- 단순히 asset을 preload하는 작업일 경우 Hook을 사용하는 것을 추천
- 단, Image.prefetch 또는 DB호출 등 다른 작업은 불가
- */
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/tabs";
 
 export default function App() {
   const [assets] = useAssets([require("./my-face.jpeg")]);
@@ -17,5 +14,9 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <Text>We are dong loading!</Text>;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
