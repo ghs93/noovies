b5b2e56b9a5cffa6fe12ea9f5d588ecf4c76d02b
hoseong <rksghtjd@gmail.com>
2021-12-06 22:46:05 +0900

    Themes

--- screens/Movies.js
+++ screens/Movies.js
@@ -5,10 +5,11 @@ const Btn = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor};
 `;
 
 const Title = styled.Text`
-  color: ${(props) => (props.selected ? "blue" : "red")};
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Movies = ({ navigation: { navigate } }) => (
