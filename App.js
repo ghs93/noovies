a378b15bb78b6b9333c458542b090699037f0a74
hoseong <rksghtjd@gmail.com>
2021-12-06 16:39:27 +0900

    Stacks and Tabs

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
-import Tabs from "./navigation/Tabs";
-import Stack from "./navigation/Stack";
+import Root from "./navigation/Root";
 
 export default function App() {
   const [assets] = useAssets([require("./my-face.jpeg")]);
@@ -17,8 +16,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      {/* <Tabs /> */}
-      <Stack />
+      <Root />
     </NavigationContainer>
   );
 }
