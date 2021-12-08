cece353b78b7f99a143f59cf986754b94ebc86f8
hoseong <rksghtjd@gmail.com>
2021-12-08 17:24:15 +0900

    Search part Two

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,5 +1,8 @@
+import { isLoaded, isLoading } from "expo-font";
 import React, { useState } from "react";
+import { useQuery } from "react-query";
 import styled from "styled-components/native";
+import { moviesApi, tvApi } from "../api";
 
 const Container = styled.ScrollView``;
 
@@ -13,7 +16,25 @@ const SearchBar = styled.TextInput`
 
 const Search = () => {
   const [query, setQuery] = useState("");
+  const {
+    isLoading: moviesLoading,
+    data: moviesData,
+    refetch: searchMovies,
+  } = useQuery(["searchMovies", query], moviesApi.search, { enabled: false });
+  const {
+    isLoading: tvLoading,
+    data: tvData,
+    refetch: searchTv,
+  } = useQuery(["searchTv", query], tvApi.search, { enabled: false });
   const onChangeText = (text: string) => setQuery(text);
+  const onSubmit = () => {
+    if (query === "") {
+      return;
+    }
+    searchMovies();
+    searchTv();
+  };
+
   return (
     <Container>
       <SearchBar
@@ -21,6 +42,7 @@ const Search = () => {
         placeholderTextColor="grey"
         returnKeyType="search"
         onChangeText={onChangeText}
+        onSubmitEditing={onSubmit}
       />
     </Container>
   );
